ee2fd7ea36d85d5eb9963102b7d90fbd636e65fe
Matteo Valles <matteovalles@gmail.com>
2018-09-09 14:05:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   var array = [element, ...array]
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  var newarray = array.push(element)
+  return newarray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var array = [...array, element]
+  return array
+}
