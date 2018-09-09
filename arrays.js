0e9ed498648ee79a23d8111f491c20ce626f4492
Matteo Valles <matteovalles@gmail.com>
2018-09-09 14:27:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,12 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+
+
+
+
